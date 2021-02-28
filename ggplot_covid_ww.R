6ed501833736a1c7fa00edeaf13cd7aa6cd425c2
VSablin <esbsablin@gmail.com>
2021-02-28 12:26:03 +0100

    Do not add 1 for linear scale.

--- ggplot_covid_ww.R
+++ ggplot_covid_ww.R
@@ -4,14 +4,17 @@ ggplot_covid_ww <- function(df,y_label,var_y,str_country,scale = "log"){
   } else {
     df_filtered <- dplyr::filter(df,country == str_country)
   }
-  p <- ggplot(df_filtered,aes(x = year_week, y = 1+{{var_y}})) +
+  p <- ggplot(df_filtered,aes(x = year_week, y = {{var_y}})) +
     geom_line() +
     # xlim(min(df_filtered$year_week),max(df_filtered$year_week)) +
     labs(y = y_label, x = "Date") +
     theme_bw()
   if (scale == "log") {
     bs <- c(1, 10, 100, 1000, 10000, 100000, 1000000, 10000000)
-    p <- p +
+    p <- ggplot(df_filtered,aes(x = year_week, y = 1 + {{var_y}})) +
+      geom_line() +
+      labs(y = y_label, x = "Date") +
+      theme_bw() +
       scale_y_continuous(trans = "log", breaks = bs, labels = bs)
   }
   return(p)
